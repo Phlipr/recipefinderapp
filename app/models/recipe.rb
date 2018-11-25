231541d4fd41f67dd19aec6b1e176faf78987d92
Phlipr <preynolds1406@gmail.com>
2018-11-25 13:16:03 -0600

    Fixed URL issue.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,12 +2,12 @@ class Recipe
   include HTTParty
 
   key_value = ENV['FOOD2FORK_KEY']
-  hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'food2fork.com'
+  hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
   base_uri "http://#{hostport}/api/search"
   default_params key: key_value
   format :json
 
   def self.for term
-    get("", query: {q: term})["recipes"]
+    get("", verify: false, query: {q: term})["recipes"]
   end
 end
